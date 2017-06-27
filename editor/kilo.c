db5acbab858299c1fa19392716c1979f71c82bf1
Andre Dias <andrediasgustavo@gmail.com>
2017-06-27 12:54:28 -0300

    Display keypress

--- editor/kilo.c
+++ editor/kilo.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -26,6 +28,12 @@ int main () {
   enableRawMode();
  
   char c;
-  while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+    if(iscntrl(c)) {
+        printf("%d\n", c); 
+    }else{
+        printf("%d ('%c')\n", c, c);
+    }
+  }
   return 0;
 }
