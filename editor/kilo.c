8d8b1ff846300e10445f45422397f87aa8710608
Andre Dias <andrediasgustavo@gmail.com>
2017-06-27 13:49:49 -0300

    Disable a bunch of flags some as CTRL+Q, CTRL+C

--- editor/kilo.c
+++ editor/kilo.c
@@ -16,8 +16,10 @@ void enableRawMode() {
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-
-  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_lflag &= ~(OPOST);
+  raw.c_lflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -30,9 +32,9 @@ int main () {
   char c;
   while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
     if(iscntrl(c)) {
-        printf("%d\n", c); 
+        printf("%d\r\n", c); 
     }else{
-        printf("%d ('%c')\n", c, c);
+        printf("%d ('%c')\r\n", c, c);
     }
   }
   return 0;
