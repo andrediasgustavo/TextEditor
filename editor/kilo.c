2714386f9f7f1cd1c16c716ae78d78e1fbc74154
Andre Dias <andrediasgustavo@gmail.com>
2017-06-29 16:47:26 -0300

    Separate in sections

--- editor/kilo.c
+++ editor/kilo.c
@@ -1,3 +1,5 @@
+/*** includes ***/
+
 #include <stdio.h>
 #include <errno.h>
 #include <ctype.h>
@@ -5,7 +7,11 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** data  ***/
+
 struct termios orig_termios;
+
+/*** terminal  ***/
 
 void die(const char *s) {
   perror(s);
@@ -33,6 +39,7 @@ void enableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+/*** init ***/
 
 int main () {
  
